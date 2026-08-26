87e2879bddab448c9aedb8f557b8b8e7bfd7fa1e
GribLeo <gribleo@yandex.ru>
2026-08-26 14:59:06 +0500

    добавил тесты для создания аккаунтов

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+User = get_user_model()
+
+
+class CustomUserManagerTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(
+            email="test@example.com", password="testpass123"
+        )
+        self.assertEqual(user.email, "test@example.com")
+        self.assertTrue(user.check_password("testpass123"))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_user_without_email(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email=None, password="testpass")
+
+    def test_create_superuser(self):
+        admin = User.objects.create_superuser(
+            email="admin@example.com", password="adminpass123"
+        )
+        self.assertTrue(admin.is_staff)
+        self.assertTrue(admin.is_superuser)
