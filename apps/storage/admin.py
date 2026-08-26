98c09a0e3e06a2f8043be151f756e631c459e8ea
Pavelwell7 <pavelbugaev303@gmail.com>
2026-08-26 12:15:17 +0300

    add storage models

--- apps/storage/admin.py
+++ apps/storage/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Warehouse
+from .models import Box
+
+@admin.register(Warehouse)
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ['city', 'address']
+    search_fields = ['city', 'address']
+
+@admin.register(Box)
+class BoxAdmin(admin.ModelAdmin):
+    list_display = ['number', 'warehouse', 'price_per_month', 'size']
+    list_filter = ['is_occupied']
