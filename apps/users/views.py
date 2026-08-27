10cb26f909561d3ac0bad1feb91cec01c6cf4b31
GribLeo <gribleo@yandex.ru>
2026-08-27 18:11:33 +0500

    сделал представление для профиля

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import UserProfileForm
+
+
+@login_required
+def profile_view(request):
+    user = request.user
+
+    has_rent = False  # TO DO добавить проверку аренды
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профиль успешно обновлён")
+            return redirect("users:profile")
+    else:
+        form = UserProfileForm(instance=user)
+        for field in form.fields.values():
+            field.widget.attrs["disabled"] = "disabled"
+
+    template = "my-rent.html" if has_rent else "my-rent-empty.html"
+    return render(request, template, {"form": form, "user": user})
